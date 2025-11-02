7fa0a943e431ee78f2be50c8e6c0e4478a00fa07
Joacim <18618554+joacimstrandvide@users.noreply.github.com>
2025-11-02 16:20:07 +0100

    flera bug fixar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
     return (
         <>
             <section className={styled.aboutMain}>
-                <h2>About Me</h2>
+                <h3>About Me</h3>
                 <h4>
                     Hello, I'm Joacim, a developer with a focus on React and
                     modern frontend apps. I also have a strong interest in
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,9 @@
-import { FiGithub, FiExternalLink } from 'react-icons/fi'
 import { projects } from '../content/projects'
 import styled from './Projects.module.css'
 
 function Projects() {
     return (
-        <main className={styled.projectsContainer}>
+        <div className={styled.projectsContainer}>
             {projects.map((project, i) => (
                 <div className={styled.projectCard} key={i}>
                     <div className={styled.projectImageContainer}>
@@ -12,51 +11,56 @@ function Projects() {
                             src={project.imagePath}
                             alt={project.name}
                             className={styled.projectImage}
-                            loading='lazy'
+                            loading="lazy"
                         />
-                        <div className={styled.techStackDesktop}>
-                            {project.techStack.split(',').map((tech, j) => (
-                                <span key={j} className={styled.techTag}>
-                                    {tech.trim()}
-                                </span>
-                            ))}
-                        </div>
                     </div>
 
                     <div className={styled.projectContent}>
-                        <h2>{project.name}</h2>
+                        <div className={styled.headerRow}>
+                            <h2>{project.name}</h2>
+                            <div className={styled.links}>
+                                {project.githubLink && (
+                                    <a
+                                        href={project.githubLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img
+                                            src="/assets/github.png"
+                                            className={styled.icon}
+                                            alt="GitHub"
+                                        />
+                                    </a>
+                                )}
+                                {project.websiteLink && (
+                                    <a
+                                        href={project.websiteLink}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        <img
+                                            src="/assets/world.png"
+                                            className={styled.icon}
+                                            alt="online"
+                                        />
+                                    </a>
+                                )}
+                            </div>
+                        </div>
+
                         <p>{project.description}</p>
-                        <div className={styled.techStackMobile}>
+
+                        <div className={styled.techStack}>
                             {project.techStack.split(',').map((tech, j) => (
                                 <span key={j} className={styled.techTag}>
                                     {tech.trim()}
                                 </span>
                             ))}
-                        </div>
-                        <div className={styled.links}>
-                            {project.githubLink && (
-                                <a
-                                    href={project.githubLink}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <FiGithub size="24px" />
-                                </a>
-                            )}
-                            {project.websiteLink && (
-                                <a
-                                    href={project.websiteLink}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                >
-                                    <FiExternalLink size="24px" />
-                                </a>
-                            )}
                         </div>
                     </div>
                 </div>
             ))}
-        </main>
+        </div>
     )
 }
 
