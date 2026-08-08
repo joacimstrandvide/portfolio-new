560e729208fa97964dadd86be77cff370ba0ee5a
Joacim <18618554+joacimstrandvide@users.noreply.github.com>
2026-08-08 10:42:42 +0200

    skrev om about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,11 +23,11 @@ export default function About() {
                         About Me
                     </h3>
                     <h4>
-                        Frontend developer from Sweden, mostly working with
-                        React and Svelte. I enjoy building useful projects, especially
-                        anything involving maps and data. I've built projects
-                        for real clients and have a background interest in
-                        cybersecurity and privacy.
+                        Frontend developer from Sweden. I started programming in
+                        2015 and mostly work with React and Svelte these days. I
+                        enjoy building useful projects, especially anything
+                        involving maps and data. I also have a longstanding
+                        interest in cybersecurity and privacy.
                     </h4>
                 </div>
                 <div className={styled.emailButtonWrapper}>
@@ -39,6 +39,9 @@ export default function About() {
                     </button>
                 </div>
             </section>
+            {showToast && (
+                <div className={styled.toast}>Email copied to clipboard!</div>
+            )}
             <Footer />
         </>
     )
